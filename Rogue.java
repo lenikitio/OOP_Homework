aba394889561fe712c62abf09f4fb6445a3f5a53
nikita <tolstoguzov.nikita98@mail.ru>
2024-01-14 04:41:54 +0300

    Second commit

--- Rogue.java
+++ Rogue.java
@@ -1,12 +1,13 @@
 public class Rogue extends Hero {
 
-    public Rogue(String nameHero) {
+    public Rogue(String nameHero, Vector2 position) {
         super(
                 100,
                 100,
                 5,
                 new int[] { 20, 30 },
-                nameHero);
+                nameHero, 
+                position.posX, position.posY);
     }
 
     @Override
