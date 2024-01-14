577ae15fe77372ff520758cc06600af49c682812
nikita <tolstoguzov.nikita98@mail.ru>
2024-01-14 04:39:13 +0300

    Second commit

--- Hero.java
+++ Hero.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * Heroes
@@ -7,14 +8,19 @@ public abstract class Hero {
     protected int health, healthMax, armor;
     protected int[] damage;
     protected String nameHero;
+    protected Vector2 position;
 
-    public Hero(int health, int healthMax, int armor, int[] damage, String nameHero){
+    public Hero(int health, int healthMax, int armor, int[] damage, String nameHero, int posX, int posY){
         this.health = health;
         this.healthMax = healthMax;
         this.armor = armor;
         this.damage = damage;
         this.nameHero = nameHero;
+        this.position = new Vector2(posX, posY);
     }
-    
 
+    public void printEnemyDistance(ArrayList<Hero> enemy){
+        enemy.forEach(n -> System.out.print(position.rangeEnemy(n.position) + ", "));
+        System.out.println();        
+    }
 }
